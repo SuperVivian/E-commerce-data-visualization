ff8e2396b579414e0d7716998a20d58658344f5d
forestcai <2801152588@qq.com>
2020-03-28 13:36:14 +0800

    remote old data_system

--- data_system/data_process/Basic/Item.py
+++
@@ -1,9 +0,0 @@
-class Item:
-
-    """商品信息表"""
-    def __init__(self,item_id = -1 ,big_cate = -1,cate_id = -1,brand_id = -1,price = 0.0):
-        self.item_id = item_id
-        self.big_cate = big_cate
-        self.cate_id = cate_id
-        self.brand_id = brand_id
-        self.price = price
--- data_system/data_process/main.py
+++
@@ -1,7 +0,0 @@
-from Control.UserControl import UserControl
-from Control.BehaviorControl import BehaviorControl
-
-print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>执行数据操作>>>>>>>>>>>>>>>>>>>>>>>>>")
-BehaviorControl()
-UserControl()
-print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>完成处理>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
